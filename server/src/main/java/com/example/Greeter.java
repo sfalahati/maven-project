1a4541b0bb524740d1e12cd150f8a649ecd49c8a
Saman <samanfal@yahoo.com>
2018-02-22 04:08:54 +0000

    hi

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,6 @@ public class Greeter {
    * This is a constructor.
    */
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Weelow, %s!", someone);
   }
 }
